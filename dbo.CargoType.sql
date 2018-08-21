044f4e27ce8dd97142cb2fab6ce9c5d86d28561c
Kashveer <Kashveer@KASHVEER-PC>
2018-08-21 12:45:36 +0200

    Added a database file

---
+++ dbo.CargoType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[CargoType] (
+    [CargoID]          VARCHAR (10) NOT NULL,
+    [CargoDescription] VARCHAR (1)  NOT NULL,
+    [CargoWeight]      VARCHAR (1)  NOT NULL,
+    [CargoHeight]      VARCHAR (1)  NOT NULL,
+    [CargoLength]      VARCHAR (1)  NOT NULL,
+    [CargoWidth]       VARCHAR (1)  NOT NULL,
+    [AbnormalLoad]     VARCHAR (5)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([CargoID] ASC)
+);
+
